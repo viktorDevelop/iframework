2e673c87ea9366e1a9475fbfd432e02c8805deac
viktorDevelop <vk.job.00@mail.ru>
2024-08-02 16:55:56 +0300

    router url validate

---
+++ app/controllers/CategoryController.php
@@ -0,0 +1,28 @@
+<?php
+namespace controllers;
+
+use http\Env\Request;
+
+class CategoryController
+{
+    public function actionIndex()
+    {
+
+    }
+
+    public function actionShow()
+    {
+
+    }
+
+    public function actionForm()
+    {
+
+    }
+
+    public function actionAdd(){}
+    public function actionEdite(){}
+    public function actionDelete(){}
+
+
+}
--- app/index.php
+++ app/index.php
@@ -1,5 +1,5 @@
 <?
-//Z2hwX3pid1V4UGoxTWFjeWVramVHaHloRkVOTUdvQnU2ejRRR1pEdQ==
+//Z2hwX29taWR1aENub3dGOHcyUHA3TFJrbUYybkxBREZzWjJheFpPQg
 include_once 'init.php';
 
 use components\Catalog;
@@ -11,8 +11,38 @@ $temp->view = new Catalog();
 $temp->setTitle('catalog');
 $temp->show();
 
+
+
 //Router::get('/');
 //Router::get('/category');
 //Router::get('/category/:alias');
 //Router::get('/category/:alias/post/:alias');
 //Router::get('/');
+
+//$url = '/category/php/php-post1';
+$url = $_SERVER['REQUEST_URI'];
+echo $url;
+
+$ar = [
+
+    '/(?P<controller>category)/(?P<cat>[a-z]+)/(?P<post>[a-z]+)/?(.*)',
+    '/(?P<controller>category)/(?P<cat>[a-z]+)/?(.*)',
+    '/(?P<controller>category)/?(.*)',
+
+    '/(?P<controller>post)/(?P<cat>[a-z]+)/(?P<post>[a-z]+)/?(.*)',
+    '/(?P<controller>post)/(?P<cat>[a-z]+)/?(.*)',
+    '/(?P<controller>post)/?(.*)',
+];
+
+
+
+
+foreach ($ar as $k=>$value)
+{
+    $r = preg_match_all("~".$value."~",$url,$match);
+    if ($r){
+        break;
+    }
+}
+echo '<pre>';
+print_r($match);
